2f31d24d7ae6849e198ca91c79585620d8a451f9
yassine <yassinemouddene@gmail.com>
2023-10-29 14:54:59 +0100

    added google authentication

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,5 @@
 import {NextAuthOptions} from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
@@ -12,22 +12,9 @@ export const options: NextAuthOptions = {
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string
         }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: {label: "Username", type: "text", placeholder: "messouab"},
-                password: {label: "Password", type: "password"},
-            },
-            async authorize(credentials) {
-                //TODO: verify credentials and return user from database
-                //  Docs: https://next-auth.js.org/configuration/providers/credentials
-                const user = {id: "1", name: "messouab",password: "messouab"};
-                if (credentials?.username === user.name && credentials?.password === user.password) {
-                    return user
-                } else {
-                    return null;
-                }
-            }
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string
         })
     ],
     session: {
