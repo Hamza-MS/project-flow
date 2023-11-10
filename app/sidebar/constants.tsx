5172cdd3eab8a99242845c8fd60bed8e749078fc
Riad_Hjm <riad.elhajjame@uir.ac.ma>
2023-11-10 16:22:57 +0100

    Update constants.tsx (Made the icons smaller)

--- app/sidebar/constants.tsx
+++ app/sidebar/constants.tsx
@@ -6,32 +6,32 @@ export const SIDENAV_ITEMS: SideNavItem[] = [
   {
     title: 'Home',
     path: '/sidebar',
-    icon: <Icon icon="lucide:home" width="24" height="24" />,
+    icon: <Icon icon="lucide:home" width="22" height="22" />,
   },
   {
     title: 'Dashboard',
     path: '/dashboard',
-    icon: <Icon icon="lucide:layout-dashboard" width="24" height="24" />,
+    icon: <Icon icon="lucide:layout-dashboard" width="22" height="22" />,
   },
   {
     title: 'Analytics',
     path: '/analytics',
-    icon: <Icon icon="mdi:analytics" width="24" height="24" />,
+    icon: <Icon icon="mdi:analytics" width="22" height="22" />,
   },
   {
     title: 'Issues',
     path: '/issues',
-    icon: <Icon icon="ant-design:issues-close-outlined" width="24" height="24" />,
+    icon: <Icon icon="ant-design:issues-close-outlined" width="22" height="22" />,
   },
   {
     title: 'Notifications',
     path: '/notifications',
-    icon: <Icon icon="ion:notifications-outline" width="24" height="24" />,
+    icon: <Icon icon="ion:notifications-outline" width="22" height="22" />,
   },
   {
     title: 'Projects',
     path: '/projects',
-    icon: <Icon icon="lucide:folder" width="24" height="24" />,
+    icon: <Icon icon="lucide:folder" width="22" height="22" />,
     submenu: true,
     subMenuItems: [
       { title: 'All', path: '/projects' },
