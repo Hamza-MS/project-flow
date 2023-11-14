d6039b361f6e951f46171aa39c0a2a65f5932915
yassine <yassinemouddene@gmail.com>
2023-11-14 11:31:49 +0100

    sidebar menu user dropdown

--- components/ui/sidebar/side-nav.tsx
+++ components/ui/sidebar/side-nav.tsx
@@ -9,6 +9,7 @@ import { SIDENAV_ITEMS } from '@/app/sidebar/constants';
 import { SideNavItem } from '@/app/sidebar/types';
 import { Icon } from '@iconify/react';
 import {WorkspaceSidebarMenu} from "@/components/ui/sidebar/sidebar-menu";
+import WorkspaceSidebarDropdown from "@/components/ui/sidebar/sidebar-dropdown";
 
 const SideNav = () => {
 
@@ -17,12 +18,12 @@ const SideNav = () => {
     return (
         <div
             id="app-sidebar"
-            className={`fixed md:relative inset-y-0 flex flex-col bg-custom-sidebar-background-100 h-full flex-shrink-0 flex-grow-0 border-r border-custom-sidebar-border-200 z-20 duration-300 ${
+            className={`fixed md:relative inset-y-0 flex flex-col bg-background h-full flex-shrink-0 flex-grow-0 border-r border-border z-20 duration-300 ${
                 sidebarCollapsed ? "" : "md:w-[280px]"
             } ${sidebarCollapsed ? "left-0" : "-left-full md:left-0"}`}
         >
           <div className="flex h-full w-full flex-1 flex-col">
-
+            <WorkspaceSidebarDropdown sidebarCollapsed={sidebarCollapsed}/>
             <WorkspaceSidebarMenu  sidebarCollapsed={sidebarCollapsed}/>
 
           </div>
---
+++ components/ui/sidebar/sidebar-dropdown.tsx
@@ -0,0 +1,67 @@
+"use client"
+import * as React from 'react';
+import {useSession} from "next-auth/react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {Button} from "@/components/ui/button";
+import {Avatar, AvatarImage, AvatarFallback} from "@/components/ui/avatar";
+import {ExitIcon, GearIcon, PersonIcon} from "@radix-ui/react-icons";
+import { useRouter } from 'next/navigation'
+import {signOut} from "next-auth/react";
+
+
+
+type Props = {
+    sidebarCollapsed: boolean;
+};
+const WorkspaceSidebarDropdown = (props: Props) => {
+
+    const {data: session} = useSession({
+        required: true,
+    })
+    const router = useRouter()
+
+    return (
+        <div className={"inline-flex items-center gap-2 px-4 pt-4"}>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant={"outline"}
+                            className={"flex items-center justify-start w-full gap-2.5 text-foreground font-medium text-sm px-1 overflow-x-clip"}>
+
+                        <Avatar className={"h-8 w-8 rounded-md"}>
+                            <AvatarImage src={session?.user?.image ?? ""}/>
+                            <AvatarFallback className={"rounded-md"}>YM</AvatarFallback>
+                        </Avatar>
+
+                        {!props.sidebarCollapsed && session?.user?.name}
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className={props.sidebarCollapsed ? "" : "md:w-[250px]"}>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator/>
+                    <DropdownMenuItem onClick={() => router.push(`/profile/${session?.user?.id}`)}>
+                        <PersonIcon className={"w-4 h-4 mr-2"}/>
+                        Profile
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => router.push(`profile/${session?.user?.id}/settings`)}>
+                        <GearIcon className={"w-4 h-4 mr-2"}/>
+                        Settings
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={() => signOut()}>
+                        <ExitIcon className={"w-4 h-4 mr-2 text-red-600"}/>
+                        <span className={"text-red-600"}>Logout</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    );
+};
+
+export default WorkspaceSidebarDropdown;
